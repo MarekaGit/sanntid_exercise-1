6ca0c4c5d32f72247cc70e0e47a7718adfc73eab
MarekaGit <martinespekarlsen@gmail.com>
2023-01-18 10:33:52 +0100

    Oppdatert foo.go og lagt til results.md

--- 3 - shared variable/go/foo.go
+++ 3 - shared variable/go/foo.go
@@ -3,29 +3,40 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+	for j := 1; j <= 1000001; j++ {
+		i += 1
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+	for j := 1; j <= 1000001; j++ {
+		i -= 1
+	}
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
-	
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// What does GOMAXPROCS do? What happens if you set it to 1?:
+	// GOMAXPROCS sets the maximum number of CPUs that can be executing
+	// simultaneously and returns the previous setting. It defaults to
+	// the value of runtime.NumCPU. If n < 1, it does not change the current setting.
+	// This call will go away when the scheduler improves.
+	runtime.GOMAXPROCS(2)
+
+	// TODO: Spawn both functions as goroutines
+
+	go incrementing()
+	go decrementing()
+
+	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+	// We will do it properly with channels soon. For now: Sleep.
+	time.Sleep(500 * time.Millisecond)
+	Println("The magic number is:", i)
 }
